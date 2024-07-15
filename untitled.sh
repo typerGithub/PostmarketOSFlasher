8586a04bc71718e71f4e47e06cf834eb4a42b3be
typerGithub <dolenko.aleksander@gmail.com>
2024-07-15 20:58:13 +0300

    Add files via upload

---
+++ untitled.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ -f ~/Downloads/20240619-1424-postmarketOS-v24.06-phosh-22.3-lenovo-a6000.img ]; then
+    echo 'Image POstmarketOS exists.'
+else
+    echo 'Image POstmarketOS does not exist.'
+fi
+
+
